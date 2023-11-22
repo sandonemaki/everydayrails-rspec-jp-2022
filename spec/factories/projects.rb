a93891e917648cb182404926ed7917419303e128
sandonemaki <kamuramaki@gmail.com>
2023-11-22 23:46:02 +0900

    factorybotのprojectにownerを関連づけ

---
+++ spec/factories/projects.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :project do
+    sequence(:name) { |n| "Project #{n}" }
+    description { "A test project." }
+    due_on { 1.week.form_now }
+    association :owner
+  end
+end
