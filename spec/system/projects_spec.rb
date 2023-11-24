fd97795e59aff34ba1ce83e9019aef27ff755b6c
sandonemaki <kamuramaki@gmail.com>
2023-11-25 01:44:49 +0900

    Capybaraのメソッド使用例コメントアウト

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -25,4 +25,28 @@ RSpec.describe "Projects", type: :system do
       expect(page).to have_content "Owner: #{user.name}"
     }.to change(user.projects, :count).by(1)
   end
+
+  ## Capybaraのメソッド使用例
+  #it "全種類のHTML要素を扱う" do
+  #  # ページを開く
+  #  visit "/fake/page"
+  #  # リンクまたはボタンのラベルをクリックする
+  #  click_on "A link or button label"
+  #  # チェックボックスのラベルをチェックする
+  #  check "A checkbox label"
+  #  # チェックボックスのラベルのチェックを外す
+  #  uncheck "A checkbox label"
+  #  # ラジオボタンのラベルを選択する
+  #  choose "A radio button label"
+  #  # セレクトメニューからオプションを選択する
+  #  select "An option", from: "A select menu"
+  #  # ファイルアップロードのラベルでファイルを添付する
+  #  attach_file "A file upload label", "/some/file/in/my/test/suite.gif"
+  #  # 指定したCSS に⼀致する要素が存在することを検証する
+  #  expect(page).to have_css "h2#subheading"
+  #  # 指定したセレクターに⼀致する要素が存在することを検証する
+  #  expect(page).to have_selector "ul li"
+  #  # 現在のパスが指定されたパスであることを検証する
+  #  expect(page).to have_current_path "/projects/new"
+  #end
 end
