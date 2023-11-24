d640d6fed9897cea6ea55dd14d89714bd86e5463
sandonemaki <kamuramaki@gmail.com>
2023-11-24 23:56:49 +0900

    postリクエストの際のcontrollerスペックを追加

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -15,4 +15,29 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
+  describe "#create" do
+    it "JSONレスポンスを返すこと" do
+      new_task = { name: "New Task" }
+      sign_in @user
+      post :create, format: :json, params: { project_id: @project.id, task: new_task }
+      expect(response.content_type).to include "application/json"
+    end
+
+    it "新しいタスクをプロジェクトに追加すること" do
+      new_task = { name: "New Task" }
+      sign_in @user
+      expect {
+        post :create, format: :json, params: { project_id: @project.id, task: new_task }
+      }.to change(@project.tasks, :count).by(1)
+    end
+
+    it "承認を要求すること" do
+      new_task = { name: "New Task" }
+      # ここではあえてログインしない
+      expect {
+        post :create, format: :json, params: { project_id: @project.id, task: new_task }
+      }.to_not change(@project.tasks, :count)
+      expect(response).to_not be_successful
+    end
+  end
 end
